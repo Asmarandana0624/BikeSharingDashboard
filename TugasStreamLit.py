35f6e21fa017d4e14c4826a7ccfc109f27d42c97
Asmarandana0624 <159117878+Asmarandana0624@users.noreply.github.com>
2025-02-12 14:21:18 +0700

    Update TugasStreamLit.py

--- TugasStreamLit.py
+++ TugasStreamLit.py
@@ -1,8 +1,7 @@
-pip install seaborn
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
+#import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
